6d90ddab0d866b8fa885960e30e4b422631fa13a
Shr199 <55866298+Shr199@users.noreply.github.com>
2019-10-03 09:45:32 +0530

    importing test project

---
+++ test-project/src/main/java/guru/springframework/test_project/App.java
@@ -0,0 +1,13 @@
+package guru.springframework.test_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
